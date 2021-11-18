af0937f8f21b25948ce838b615210389f17b2b1c
Ammar Faizi <ammarfaizi2@gmail.com>
2021-11-18 17:17:34 +0700

    Fix MovieDetail
    Signed-off-by: Ammar Faizi <ammarfaizi2@gmail.com>

--- app/src/main/java/com/example/moviecatalog/MovieDetail.java
+++ app/src/main/java/com/example/moviecatalog/MovieDetail.java
@@ -1,51 +1,28 @@
 package com.example.moviecatalog;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.os.Bundle;
-
-import com.google.android.material.snackbar.Snackbar;
+import android.util.Log;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.view.View;
-
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
-
-import com.example.moviecatalog.databinding.ActivityMovieDetailBinding;
+import java.io.InputStream;
 
 public class MovieDetail extends AppCompatActivity {
-
-    private AppBarConfiguration appBarConfiguration;
-    private ActivityMovieDetailBinding binding;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        binding = ActivityMovieDetailBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
-
-        setSupportActionBar(binding.toolbar);
-
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_movie_detail);
-        appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-
-        binding.fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-    }
-
-    @Override
-    public boolean onSupportNavigateUp() {
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_movie_detail);
-        return NavigationUI.navigateUp(navController, appBarConfiguration)
-                || super.onSupportNavigateUp();
+        setContentView(R.layout.activity_movie_detail);
+        TextView title = findViewById(R.id.movieTitleDetail);
+        TextView overview = findViewById(R.id.movieOverviewDetail);
+        TextView releaseDate = findViewById(R.id.movieReleaseDateDetail);
+        title.setText(CustomAdapter.title_);
+        overview.setText(CustomAdapter.overview_);
+        releaseDate.setText(CustomAdapter.releaseDate_);
+        new DownloadImageTask(findViewById(R.id.moviePicDetail)).execute(CustomAdapter.pic_);
     }
 }
