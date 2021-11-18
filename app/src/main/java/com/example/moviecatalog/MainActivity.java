770252be7668ea18463753a881d377e8d0316f5a
Ammar Faizi <ammarfaizi2@gmail.com>
2021-11-18 17:27:55 +0700

    Delete unused attribute and test setTitle()
    Signed-off-by: Ammar Faizi <ammarfaizi2@gmail.com>

--- app/src/main/java/com/example/moviecatalog/MainActivity.java
+++ app/src/main/java/com/example/moviecatalog/MainActivity.java
@@ -32,6 +32,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setTitle("Movie Directory");
         pics = new ArrayList<Integer>();
         listData = new ArrayList<String[]>();
         getDataFromAPI();
